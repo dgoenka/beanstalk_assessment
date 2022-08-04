85b830bdb5c2e17a34c2a723a9a770d557bf5506
Divyansh Goenka <divyansh.goenka@gmail.com>
2022-08-04 18:23:16 +0530

    - File download working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { useDropzone } from 'react-dropzone';
 import { useEffect } from 'react';
 import { API_URL } from './config/api';
+import { downloadTextFile } from './util/download';
+
 function App() {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     multiple: false,
@@ -28,24 +30,23 @@ function App() {
           requestOptions,
         )).json();
 
-        const dataStr =
-          'data:text/json;charset=utf-8,' +
-          encodeURIComponent(JSON.stringify(result.data));
-        const dlAnchorElem = document.getElementById('downloadAnchorElem');
-        dlAnchorElem.setAttribute('href', dataStr);
-        dlAnchorElem.setAttribute('download', `${acceptedFiles[0].name}.json`);
-        dlAnchorElem.click();
+        downloadTextFile(
+          JSON.stringify(result.data, null, 2),
+          `${acceptedFiles[0].name}.json`,
+        );
       })();
     }
   }, [acceptedFiles]);
 
   return (
-    <div className="App">
-      <div {...getRootProps({ className: 'dropzone w100 h100 center' })}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop or click to select a a CSV file here</p>
+    <>
+      <div className="App">
+        <div {...getRootProps({ className: 'dropzone w100 h100 center' })}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop or click to select a a CSV file here</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/util/download.js
@@ -0,0 +1,9 @@
+export const downloadTextFile = (text, name) => {
+  const a = document.createElement('a');
+  const type = name.split('.').pop();
+  a.href = URL.createObjectURL(
+    new Blob([text], { type: `text/${type === 'txt' ? 'plain' : type}` }),
+  );
+  a.download = name;
+  a.click();
+};
