2ff86374236702c8ede63013e6c7049c2cf4422c
Divyansh Goenka <divyansh.goenka@gmail.com>
2022-08-04 18:17:29 +0530

    - Added basic frontend functionality

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import { useDropzone } from 'react-dropzone';
+import { useEffect } from 'react';
+import { API_URL } from './config/api';
+function App() {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    multiple: false,
+    maxFiles: 1,
+    accept: {
+      'text/*': ['.txt', '.csv'],
+    },
+  });
+
+  useEffect(() => {
+    if (acceptedFiles[0]) {
+      (async () => {
+        const formdata = new FormData();
+        formdata.append('file', acceptedFiles[0], acceptedFiles[0].name);
+
+        const requestOptions = {
+          method: 'POST',
+          body: formdata,
+          redirect: 'follow',
+        };
+
+        const result = await (await fetch(
+          `${API_URL}transform/csv`,
+          requestOptions,
+        )).json();
+
+        const dataStr =
+          'data:text/json;charset=utf-8,' +
+          encodeURIComponent(JSON.stringify(result.data));
+        const dlAnchorElem = document.getElementById('downloadAnchorElem');
+        dlAnchorElem.setAttribute('href', dataStr);
+        dlAnchorElem.setAttribute('download', `${acceptedFiles[0].name}.json`);
+        dlAnchorElem.click();
+      })();
+    }
+  }, [acceptedFiles]);
+
+  return (
+    <div className="App">
+      <div {...getRootProps({ className: 'dropzone w100 h100 center' })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop or click to select a a CSV file here</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/config/api.js
@@ -0,0 +1,3 @@
+console.log(' process.env is:\n' + JSON.stringify(process.env, null, 2));
+
+export const API_URL = process.env.REACT_APP_API_URL;
